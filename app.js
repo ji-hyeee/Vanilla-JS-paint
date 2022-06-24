c2c3c0953158cea38981e998dc273a1b697e277c
sulmeee <poorooria@naver.com>
2022-06-24 15:44:25 +0900

    changing color

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-// #2.2 Recap! 
+// #2.3 Changing Color 
 const canvas = document.getElementById("jsCanvas");
-
 const ctx = canvas.getContext("2d");
-// 캔버스에 사이즈 주기
-// 2개의 사이즈가 필요하다 css 사이즈 + pixel manipulating 사이즈 <<< 이걸 지정해주기
-canvas.width = 700; // 실제 픽셀 사이즈 ~~~ !
+
+// path color
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 700;
 canvas.height = 700;
-
-// context 색깔
 ctx.strokeStyle = '#2c2c2c'
-
-// context 선 굵기
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -23,27 +20,37 @@ function startPainting() {
     painting = true;
 }
 
-// 마우스는 항상 움직이고 있고 안 보이는 상태로 path를 만들고 있는 것 !
 function onMouseMove(e) {
     const x = e.offsetX;
     const y = e.offsetY;
     if (!painting) {
-        // path === line
-        console.log("creating path in ", x, y); // 확인용 콘솔
+        // console.log("creating path in ", x, y);
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
-        console.log("creating line in ", x, y); // 확인용 콘솔
-        // 마우스를 움직이다가 마우스를 클릭하고 이어서 움직이면
-        // 화면에서 path 의 시작점부터 선이 생기기 시작해
+        // console.log("creating line in ", x, y);
         ctx.lineTo(x, y);
-        // stroke 으로 진짜 화면에 보일것이다 
         ctx.stroke();
     }
 };
 
-function onMouseDown(e) {
-    painting = true;
+// function onMouseDown(e) {
+//     painting = true;
+// }
+
+// 색상변경 이벤트 함수
+function changeColorClick(e) {
+    // console.log(e.target.style);
+    // 요소를 선택할 때마다 정보를 콘솔에서 확인할 수 있다
+    // 그 많은 정보들 중 우리가 사용하고 싶은 건 
+    // backgroundColor ~
+    const color = e.target.style.backgroundColor;
+    console.log(color);
+
+    // 이제 context color 를 변경해야 한다
+    // 지금은 검은색 비스무리한 걸로 default 값이 지정되어 있다
+    // strokeStyle 을 override 하기 / 덮어쓰기
+    ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -52,3 +59,17 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+// console.log(colors); // <<< HTML collection 이 나온다
+// Array.from 메소드는 object로부터 array 를 만든다
+// console.log(Array.from(colors));
+
+// 배열의 요소에 이벤트 만들어주기
+Array.from(colors).forEach(el => el.addEventListener("click", changeColorClick));
+
+// 이번에 배운 것
+// 배열을 만들고
+// 그 배열에 이벤트를 만들고
+// 이벤트 함수에서 색상 타겟을 가져오고
+// 그 타켓을 context 에 연결해줬다
+// wow
