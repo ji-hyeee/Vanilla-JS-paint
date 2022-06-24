da6ae8329f04fb77cdb4fbfc33021ebbcabba86d
sulmeee <poorooria@naver.com>
2022-06-24 16:02:33 +0900

    brush size

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// #2.3 Changing Color 
+// #2.4 Brush Size
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-
-// path color
 const colors = document.getElementsByClassName("jsColor");
+
+// 선 굵기 변경하기
+const range = document.getElementById("jsRange");
+
+// fill 버튼을 누르면 text 가 paint 로 바뀌도록 
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -11,6 +15,9 @@ ctx.strokeStyle = '#2c2c2c'
 ctx.lineWidth = 2.5;
 
 let painting = false;
+
+// 캔버스를 fill 할 변수 선언
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -24,33 +31,37 @@ function onMouseMove(e) {
     const x = e.offsetX;
     const y = e.offsetY;
     if (!painting) {
-        // console.log("creating path in ", x, y);
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
-        // console.log("creating line in ", x, y);
         ctx.lineTo(x, y);
         ctx.stroke();
     }
 };
 
-// function onMouseDown(e) {
-//     painting = true;
-// }
-
-// 색상변경 이벤트 함수
 function changeColorClick(e) {
-    // console.log(e.target.style);
-    // 요소를 선택할 때마다 정보를 콘솔에서 확인할 수 있다
-    // 그 많은 정보들 중 우리가 사용하고 싶은 건 
-    // backgroundColor ~
     const color = e.target.style.backgroundColor;
-    console.log(color);
-
-    // 이제 context color 를 변경해야 한다
-    // 지금은 검은색 비스무리한 걸로 default 값이 지정되어 있다
-    // strokeStyle 을 override 하기 / 덮어쓰기
     ctx.strokeStyle = color;
+}
+
+function handleRangeChange(e) {
+    // console.log(e)
+    // input 의 여러 정보 중 value 값을 얻자
+    // console.log(e.target.value)
+    // path 에 적용하기
+    const size = e.target.value;
+    ctx.lineWidth = size;
+}
+
+// 버튼을 click 하면 text 가 바뀌는 이벤트함수
+function handleModeClick(e) {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
 }
 
 if (canvas) {
@@ -60,16 +71,16 @@ if (canvas) {
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
-// console.log(colors); // <<< HTML collection 이 나온다
-// Array.from 메소드는 object로부터 array 를 만든다
-// console.log(Array.from(colors));
 
-// 배열의 요소에 이벤트 만들어주기
 Array.from(colors).forEach(el => el.addEventListener("click", changeColorClick));
 
-// 이번에 배운 것
-// 배열을 만들고
-// 그 배열에 이벤트를 만들고
-// 이벤트 함수에서 색상 타겟을 가져오고
-// 그 타켓을 context 에 연결해줬다
-// wow
+if (range) {
+    range.addEventListener("input", handleRangeChange)
+}
+
+// fill 버튼도 활성화해보자
+// 색상을 선택하고 컨버스를 누르면 그 색깔로 다 채워지게끔
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
+}
